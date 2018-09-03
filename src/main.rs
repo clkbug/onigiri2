d3405424162ee457cd040b4a3b0ee206d2e4fbc8
Akifumi Fukuda <a.fukuda@mtl.t.u-tokyo.ac.jp>
2018-09-03 17:36:54 +0900

    Add operation type

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,84 @@ struct ArchitectureState {
 }
 
 struct DecodedInstruction {
-    opcode: u32,
+    operation: Operation,
     dstRegs: Vec<i32>,
     srcRegs: Vec<i32>,
     imm: i32,
 }
 
-fn decode(codeWord: u32) -> DecodedInstruction {
-    let opcode = codeWord & 0x32;
+enum Operation {
+    LUI,
+    AUIPC,
+    JAL,
+    JALR,
+    BEQ,
+    BNE,
+    BLT,
+    BGE,
+    BLTU,
+    BGEU,
+    LB,
+    LH,
+    LW,
+    LBU,
+    LHU,
+    SB,
+    SH,
+    SW,
+    ADDI,
+    SLTI,
+    SLTIU,
+    XORI,
+    ORI,
+    ANDI,
+    SLLI,
+    SRLI,
+    SRAI,
+    ADD,
+    SUB,
+    SLL,
+    SLT,
+    SLTU,
+    XOR,
+    SRL,
+    SRA,
+    OR,
+    AND,
+    FENCE,
+    FENCEI,
+    ECALL,
+    EBREAK,
+    CSRRW,
+    CSRRS,
+    CSRRC,
+    CSRRWI,
+    CSRRSI,
+    CSRRCI,
+    NOP,
+    UNDEFINED, // for debug
+}
+
+fn decode_u_type(code_word: u32) -> DecodedInstruction {
+    let opcode = code_word & 0x7f;
     return DecodedInstruction {
-        opcode: opcode,
+        operation: Operation::LUI,
         dstRegs: vec![1, 2],
         srcRegs: vec![1],
         imm: 0,
+    };
+}
+
+fn decode(code_word: u32) -> DecodedInstruction {
+    let opcode = code_word & 0x7f;
+    return match opcode {
+        0x38 => decode_u_type(code_word),
+        _ => DecodedInstruction {
+            operation: Operation::UNDEFINED,
+            dstRegs: vec![0],
+            srcRegs: vec![0],
+            imm: 1,
+        },
     };
 }
 fn main() {
@@ -25,4 +90,5 @@ fn main() {
         regs: [0; 32],
     };
     println!("Hello, world! {}", arch_state.pc);
+    println!("{}", decode(0).imm);
 }
